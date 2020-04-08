9b0ac9519fcbed7862820cd12b1f631878c37fca
crym761224 <37447017+crym761224@users.noreply.github.com>
2020-04-09 00:57:24 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -9,3 +9,5 @@
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
 sed -i 's/192.168.1.1/192.168.0.1/g' package/base-files/files/bin/config_generate
 git clone https://github.com/tty228/luci-app-serverchan.git
+./scripts/feeds update -a
+./scripts/feeds install -a
